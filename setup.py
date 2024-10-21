be9a49fe414472253653cb4ee2820a8e5c09403a
Dmytro Mykhailenko <mismartconfig@gmail.com>
2024-10-21 13:27:24 +0300

    relax version specs for dependencies

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ setup(
     url='https://pypi.org/project/dm-aioinfluxdb',
     packages=find_packages(),
     install_requires=[
-        'aiohttp==3.9.2',
-        'aiocsv==1.2.5',
-        'dm-logger==0.5.2',
-        'influxdb-client==1.39.0'
+        'dm-logger~=0.5.2',
+        'aiohttp>=3.9.2, <4.0.0',
+        'aiocsv>=1.2.5, <2.0.0',
+        'influxdb-client~=1.39.0'
     ],
     classifiers=[
         'Programming Language :: Python :: 3.8',
